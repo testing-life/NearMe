5594b9dc1ace830c16cf82c626a7cffc7758fc07
Mariusz <mariusz.pankiewicz@diva-e.com>
2024-01-03 11:26:55 +0100

    Auto stash before merge of "feature/design-implementation" and "origin/feature/design-implementation"

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,11 +1,11 @@
-import { browserSessionPersistence } from "firebase/auth";
-import React, { useEffect } from "react";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link, useNavigate } from "react-router-dom";
-import Login from "../Components/Login/Login";
-import { HOME, RESET, SIGN_UP } from "../Consts/Routes";
-import { auth } from "../Firebase/Firebase";
-import "./LoginPage.css";
+import { browserSessionPersistence } from 'firebase/auth';
+import React, { useEffect } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import Login from '../Components/Login/Login';
+import { HOME, RESET, SIGN_UP } from '../Consts/Routes';
+import { auth } from '../Firebase/Firebase';
+import './LoginPage.css';
 
 const LoginPage = () => {
   const [signInWithEmailAndPassword, user, loading, error] =
@@ -24,31 +24,27 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="login-page">
-      <div className="row">
+    <div className='login-page'>
+      <div className='row'>
         <Login submitHandler={onSubmit} />
         {error && <p>{error.message}</p>}
       </div>
       {loading && (
-        <div className="row">
+        <div className='row'>
           <p>Signing in...</p>
         </div>
       )}
-      <div className="row">
-        <p>
-          No account?
-          <Link className="" to={SIGN_UP}>
-            Sign up
-          </Link>
-        </p>
+      <div className='column --is-centred-column --has-spearator'>
+        <p>No account?</p>
+        <Link className='' to={SIGN_UP}>
+          Sign up
+        </Link>
       </div>
-      <div className="row">
-        <p>
-          Forgot your password ?
-          <Link className="" to={RESET}>
-            Reset password
-          </Link>
-        </p>
+      <div className='column --is-centred-column'>
+        <p>Forgot your password ?</p>
+        <Link className='' to={RESET}>
+          Reset password
+        </Link>
       </div>
     </div>
   );
