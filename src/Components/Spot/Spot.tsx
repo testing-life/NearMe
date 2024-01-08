ecdd3ca9bbdbd62dc089e4fcb25f5e49c13ef466
Mariusz <mariusz.pankiewicz@diva-e.com>
2024-01-08 09:48:32 +0100

    default img

--- src/Components/Spot/Spot.tsx
+++ src/Components/Spot/Spot.tsx
@@ -1,7 +1,8 @@
-import React, { FC, ReactNode } from "react";
-import { ISpot } from "../../Models/spot";
-import "./Spot.css";
-import PillsList from "../PillsList/PillsList";
+import React, { FC, ReactNode } from 'react';
+import { ISpot } from '../../Models/spot';
+import './Spot.css';
+import PillsList from '../PillsList/PillsList';
+import noImage from './../../Assets/no-img.svg';
 interface Props {
   spot: ISpot;
   children?: ReactNode;
@@ -9,26 +10,25 @@ interface Props {
 
 const Spot: FC<Props> = ({ spot, children }) => {
   return (
-    <article className="spot">
-      <div className="spot__tools">{children}</div>
-
-      {spot.poster.url ? (
-        <div className="spot__image">
-          <img src={spot.poster.url} alt="" />
-        </div>
-      ) : null}
-
-      <div className="spot__overlay">
-        <div className="spot__tags">
+    <article className='spot'>
+      <div className='spot__tools'>{children}</div>
+      <div className='spot__image'>
+        <img
+          src={spot.poster.url ? spot.poster.url : noImage}
+          alt='No image available'
+        />
+      </div>
+      <div className='spot__overlay'>
+        <div className='spot__tags'>
           <PillsList labels={spot.tags} />
         </div>
-        <div className="spot__details">
-          <p className="title">{spot.name}</p>
-          <span className="subtitle">{spot.address}</span>
+        <div className='spot__details'>
+          <p className='title'>{spot.name}</p>
+          <span className='subtitle'>{spot.address}</span>
         </div>
         {spot.notes ? (
-          <div className="spot__notes">
-            <p className="leading-tight">
+          <div className='spot__notes'>
+            <p className='leading-tight'>
               <span>{spot.notes}</span>
             </p>
           </div>
