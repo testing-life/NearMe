92834abd55608ff7c419bd788d4f7c3b69d4c95d
M <aethelbrihte@gmail.com>
2024-01-09 21:27:31 +0100

    drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import * as ROUTES from './Consts/Routes';
-import SignupPage from './Pages/SignupPage';
-import HomePage from './Pages/HomePage';
-import LoginPage from './Pages/LoginPage';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import AddSpotPage from './Pages/AddSpotPage';
+import React from "react";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import * as ROUTES from "./Consts/Routes";
+import SignupPage from "./Pages/SignupPage";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
+import AddSpotPage from "./Pages/AddSpotPage";
 // import "cirrus-ui";
-import UpdateCredentialsPage from './Pages/UpdateCredentialsPage';
-import ResetPasswordPage from './Pages/ResetPasswordPage';
-import EditSpotPage from './Pages/EditSpotPage';
-import Drawer from './Components/Drawer/Drawer';
-import useDrawerStore from './Stores/drawerStore';
-import { useSignOut } from 'react-firebase-hooks/auth';
-import { auth } from './Firebase/Firebase';
+import UpdateCredentialsPage from "./Pages/UpdateCredentialsPage";
+import ResetPasswordPage from "./Pages/ResetPasswordPage";
+import EditSpotPage from "./Pages/EditSpotPage";
+import Drawer from "./Components/Drawer/Drawer";
+import useDrawerStore from "./Stores/drawerStore";
+import { auth } from "./Firebase/Firebase";
 
 function App() {
   const drawerOpen = useDrawerStore((state) => state.isOpen);
-  const [signOut, loading, error] = useSignOut(auth);
+
   return (
-    <div className='wrapper'>
-      <Drawer isOpen={drawerOpen}>
-        <nav>
-          <ul>
-            <li>
-              <button onClick={async () => await signOut()}>Log out</button>
-            </li>
-            <li>
-              <Link className='text-light' to={ROUTES.UPDATE}>
-                Update Details
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </Drawer>
+    <div className="wrapper">
+      <Drawer isOpen={drawerOpen} user={auth} />
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <PrivateRoute>
               <HomePage />
--- src/Components/Drawer/Drawer.tsx
+++ src/Components/Drawer/Drawer.tsx
@@ -1,20 +1,38 @@
-import React, { FC, ReactNode } from 'react';
-import './Drawer.css';
-import DrawerButton from '../DrawerButton/DrawerButton';
-import useDrawerStore from '../../Stores/drawerStore';
+import React, { FC, ReactNode } from "react";
+import "./Drawer.css";
+import DrawerButton from "../DrawerButton/DrawerButton";
+import useDrawerStore from "../../Stores/drawerStore";
+import { Auth, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import * as ROUTES from "./../../Consts/Routes";
+import { useSignOut } from "react-firebase-hooks/auth";
 
 interface Props {
   isOpen: boolean;
-  children: ReactNode;
+  user: Auth;
 }
 
-const Drawer: FC<Props> = ({ isOpen, children }) => {
+const Drawer: FC<Props> = ({ isOpen, user }) => {
   const toggleDrawer = useDrawerStore((state) => state.toggleDrawer);
-
+  const [signOut, loading, error] = useSignOut(user);
   return (
-    <div className={`drawer ${isOpen ? '-is-open' : ''}`}>
-      <DrawerButton isClose onClick={() => toggleDrawer(true)} />
-      {children}
+    <div className={`drawer__wrapper ${isOpen ? "-is-open" : ""}`}>
+      <div className="drawer ">
+        <DrawerButton isClose onClick={() => toggleDrawer(true)} />
+        <nav className="-sticky-top">
+          <ul>
+            <li>
+              <button onClick={async () => await signOut()}>Log out</button>
+            </li>
+            <li>
+              <Link className="text-light" to={ROUTES.UPDATE}>
+                Update Details
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* <div className="drawer__overlay"></div> */}
     </div>
   );
 };
